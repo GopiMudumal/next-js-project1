e35e9ccef0643a519a4facc70ecbda5c75831666
GopiMudumal <gopi.mudumal@perennialsys.com>
2022-07-21 16:34:24 +0530

    using getStaticPath function here to give the daynamic paths name

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -4,8 +4,49 @@ import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 const MeetupDetails = () => {
   return (
-   <MeetupDetail image ="https://w0.peakpx.com/wallpaper/621/172/HD-wallpaper-allu-arjun-allu-arjun-thumbnail.jpg" title= "A first meet up" address="Some city some place" description = "new to this"/>
+    <MeetupDetail
+      image="https://w0.peakpx.com/wallpaper/621/172/HD-wallpaper-allu-arjun-allu-arjun-thumbnail.jpg"
+      title="A first meet up"
+      address="Some city some place"
+      description="new to this"
+    />
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    fallback:false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // feacth data for single
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://w0.peakpx.com/wallpaper/621/172/HD-wallpaper-allu-arjun-allu-arjun-thumbnail.jpg",
+        id: meetupId,
+        title: "A first meet up",
+        address: "Some city some place",
+        description: "new to this but this is brand",
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
